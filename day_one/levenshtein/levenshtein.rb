e62831dca536687b175d773ddd05a6f297db9a3b
Schocker <adikorzeniowski@gmail.com>
2018-07-09 22:18:56 +0200

    Cleanup formatting

--- day_one/levenshtein/levenshtein.rb
+++ day_one/levenshtein/levenshtein.rb
@@ -10,18 +10,21 @@ def levenshtein(first_word, second_word)
   if first_word == second_word
     return 0
   else 
-    distances = Matrix.zero(first_word.size+1, second_word.size+1)
-    for row in 0...first_word.size+1
+    rows = first_word.size + 1
+    cols = second_word.size + 1
+
+    distances = Matrix.zero(rows, cols)
+    for row in 0...rows
       distances[row, 0] = row
     end
 
-    for col in 0...second_word.size+1
+    for col in 0...cols
       distances[0, col] = col
     end
 
-    for row in 1...first_word.size+1
-      for col in 1...second_word.size+1
-        cost = first_word[row-1] == second_word[col-1] ? 0 : 1
+    for row in 1...rows
+      for col in 1...cols
+        first_word[row - 1] == second_word[col - 1] ? cost = 0 : cost = 1
         distances[row, col] = [ distances[row - 1, col] + 1,
                               distances[row, col - 1] + 1,
                               distances[row - 1, col - 1] + cost].min
