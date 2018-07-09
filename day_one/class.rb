537a569f51ee9a0817f62cdde03c3be963b28046
Schocker <adikorzeniowski@gmail.com>
2018-07-09 14:12:48 +0200

    Implement handling of more typical case of levenshtein

--- day_one/class.rb
+++ day_one/class.rb
@@ -10,6 +10,12 @@ class Animal
   end
 end 
 
+class Dog < Animal
+  def give_sound()
+    puts 'hau'
+  end
+end
+
 animal = Animal.new('Reksio', 4)
 # animal.name = 'Reksio'
 # animal.age = '4'
@@ -20,3 +26,14 @@ puts animal.age
 
 animal.give__sound("szczek")
 animal.give__sound("miau")
+
+class Mammal
+end
+ 
+class LivesInSea
+end
+
+class Whale
+  include Mammal
+  include LivesInSear
+end
--- day_one/levenshtein/levenshtein.rb
+++ day_one/levenshtein/levenshtein.rb
@@ -2,12 +2,16 @@ def levenshtein(first_word, second_word)
   if first_word == second_word
     distance = 0
   else 
-    if [first_word.size, second_word.size].min == 0
-      distance = [first_word.size, second_word.size].max
+    lengths = [first_word.size, second_word.size]
+    if lengths.min == 0
+      distance = lengths.max
     else
-      distance = 1        
+      distance = [levenshtein(first_word.size - 1, second_word.size) + 1,
+                  levenshtein(first_word.size, second_word.size - 1) + 1,
+                  levenshtein(first_word.size - 1, second_word.size - 1) + first_word.size == second_word.size ? 0 : 1]  
+    end
   end
   return distance
 end
 
-levenshtein('cat', 'hat')
+puts levenshtein('cat', 'hat')
