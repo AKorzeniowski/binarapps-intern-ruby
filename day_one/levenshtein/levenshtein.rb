fc0f7fcf6f181a41ebd67d908cc73e8cf634d0c2
Schocker <adikorzeniowski@gmail.com>
2018-07-09 22:49:54 +0200

    Fix handling of nil values in levenshtein, make use of command line arguments

--- day_one/levenshtein/levenshtein.rb
+++ day_one/levenshtein/levenshtein.rb
@@ -9,6 +9,10 @@ end
 def levenshtein(first_word, second_word)
   if first_word == second_word
     return 0
+  elsif first_word == nil
+    return second_word.size
+  elsif second_word == nil
+    return first_word.size
   else 
     rows = first_word.size + 1
     cols = second_word.size + 1
@@ -26,8 +30,8 @@ def levenshtein(first_word, second_word)
       for col in 1...cols
         first_word[row - 1] == second_word[col - 1] ? cost = 0 : cost = 1
         distances[row, col] = [ distances[row - 1, col] + 1,
-                              distances[row, col - 1] + 1,
-                              distances[row - 1, col - 1] + cost].min
+                                distances[row, col - 1] + 1,
+                                distances[row - 1, col - 1] + cost].min
       end
     end
 
@@ -35,4 +39,4 @@ def levenshtein(first_word, second_word)
   end
 end
 
-puts levenshtein('cats', 'hat')
+puts levenshtein(ARGV[0], ARGV[1])
