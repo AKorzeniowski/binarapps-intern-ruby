8eba63b6cd4933664160458cc718349301ff2943
Schocker <adikorzeniowski@gmail.com>
2018-07-10 10:37:08 +0200

    Make 99 bottles return words for last 5 bottles

--- day_one/99bottles/99bottles.rb
+++ day_one/99bottles/99bottles.rb
@@ -1,6 +1,15 @@
-99.downto(1).each do |bottle_number|
-  puts "#{bottle_number} bottles of beer on the wall, #{bottle_number} bottles of beer.
-  Take one down and pass it around, #{bottle_number-1} bottles of beer on the wall.\n\n"
+def wordize_number(number)
+  worded_numbers = { 5 => "five", 4  => "four", 3  => "three", 2  => "two", 1  => "one", 0  => "no more"}
+  if worded_numbers.key?(number)
+    return worded_numbers[number]
+  else
+    return number
+  end 
+end
+
+99.downto(1) do |bottle_number|
+  pluralized_item = bottle_number > 1 ? 'bottles' : 'bottle'
+  puts "#{wordize_number(bottle_number)} #{pluralized_item} of beer on the wall, #{wordize_number(bottle_number)} #{pluralized_item} of beer.\nTake one down and pass it around, #{wordize_number(bottle_number-1)} #{pluralized_item} of beer on the wall.\n\n"
 end
 
 puts "No more bottles of beer on the wall, no more bottles of beer. 
